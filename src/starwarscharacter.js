e0810939147dece04ee187c137168109f89cb7bb
Keyner <keyner.gp.steff@gmail.com>
2020-11-27 20:23:16 -0600

    Alert done

---
+++ src/starwarscharacter.js
@@ -0,0 +1,6 @@
+import './App.css';
+import React from 'react'
+
+export default class starwarscharacter extends React.Component {
+    
+}
---
+++ src/starwarspeople.js
@@ -0,0 +1,6 @@
+import './App.css';
+import React from 'react'
+
+export default class starwarspeople extends React.Component {
+    
+}
